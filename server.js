35d78dfba589f228ae7532e2b1a642f82901f5fc
Pete Brousalis <brousapg@gmail.com>
2021-05-21 14:16:15 -0500

    lol

--- server.js
+++ server.js
@@ -14,29 +14,19 @@ var main = async () => {
   });
   var server = http.createServer(app);
   server.listen(port);
-  console.log(`
-    ____        __          __          _____                          
-   / __ )____ _/ /_  __  __/ /___  ____/ ___/___  ______   _____  _____
-  / __  / __ \`/ __ \\/ / / / / __ \\/ __ \\\__ \\/ _ \\/ ___/ | / / _ \\/ ___/
- / /_/ / /_/ / /_/ / /_/ / / /_/ / / / /__/ /  __/ /  | |/ /  __/ /    
-/_____/\\__,_/_.___/\\__, /_/\\____/_/ /_/____/\\___/_/   |___/\\___/_/     
-                  /____/                                                
-    `);
+
   console.log("Serving at: localhost:" + port + "/pic.png");
 
   // Puppeteer ---------------------------------------------------------------------------------
   const browser = await puppeteer.launch({});
   const page = await browser.newPage();
-  await page.goto("https://playground.babylonjs.com/frame.html#PN1NNI#1"); // Or localhost:3000
+  // await page.goto("https://playground.babylonjs.com/frame.html#PN1NNI#1"); // Or localhost:3000
+  await page.goto("http://localhost:3000"); // Or localhost:3000
 
   // Fullscreen canvas element
   page.evaluate("document.getElementsByTagName('canvas')[0].style.zIndex=500");
-  page.evaluate(
-    " document.getElementsByTagName('canvas')[0].style.height='100%'"
-  );
-  page.evaluate(
-    "document.getElementsByTagName('canvas')[0].style.position='absolute'"
-  );
+  page.evaluate(" document.getElementsByTagName('canvas')[0].style.height='100%'");
+  page.evaluate("document.getElementsByTagName('canvas')[0].style.position='absolute'");
 
   // Take a screenshot, save it locally and serve it to the requested webpage, need to wait a bit for the webpage to load before calling
   app.get("/pic.png", async (req, res) => {
